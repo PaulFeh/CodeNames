a7cf326328af3d86bf142ac9be9ee1602f586d46
Viet <vnguyen.fl@gmail.com>
2020-12-28 11:03:29 -0500

    remove unused rand int function

--- src/app/util.ts
+++ src/app/util.ts
@@ -1,9 +1,3 @@
-export function getRandomInt(min: number, max: number): number {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 export function randomString(length: number, chars: string): string {
   let result = '';
   for (let i = length; i > 0; --i) {
