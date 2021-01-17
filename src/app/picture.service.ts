a6735a7e53550dd3b4ce3a4fe36d2e9d5c8068e7
Viet <vnguyen.fl@gmail.com>
2021-01-17 16:41:06 -0500

    remove dupe

--- src/app/picture.service.ts
+++ src/app/picture.service.ts
@@ -8,7 +8,7 @@ export class PictureService {
   constructor() {}
 
   getImages(total = 20, previousCards?: number[]): string[] {
-    const totalImages = 325;
+    const totalImages = 324;
     let imgArray = Array.from(Array(totalImages), (e, i) => i + 1);
     imgArray = shuffle(imgArray);
 
